ad8451f287fc3d66939d6f3d42666e48ffbaedc6
Paulo henrique <phsantosferreira9@gmail.com>
2021-11-11 10:03:36 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/loja/celular/CelularApplication.java
@@ -0,0 +1,13 @@
+package br.com.loja.celular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/loja/celular/CelularApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.loja.celular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
